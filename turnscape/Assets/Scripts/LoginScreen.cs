92f7032e6683cd5e33c701b64e03806330107bce
Armisb <armbil@ktu.lt>
2026-02-22 14:08:00 +0200

    Added login checks

--- turnscape/Assets/Scripts/LoginScreen.cs
+++ turnscape/Assets/Scripts/LoginScreen.cs
@@ -1,12 +1,19 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class ChangeInput : MonoBehaviour
 {
     private EventSystem system;
+    [SerializeField] private TMP_InputField mailField, passwordField;
+    [SerializeField] private TMP_Text errorMessage; 
+    
+  
 
     private void Start()
     {
@@ -16,7 +23,23 @@ public class ChangeInput : MonoBehaviour
     private void Update()
     {
         HandleTabbingThroughFields();
+        
     }
+
+    public void TextFieldFilled()
+    {
+        if (string.IsNullOrWhiteSpace(mailField.text) || string.IsNullOrWhiteSpace(passwordField.text))
+        {
+            Debug.Log("email or password is empty");
+            errorMessage.text = "Mail or password is empty!";
+            errorMessage.gameObject.SetActive(true);
+        }
+        else
+        {
+            GameManagerSc.LoadScene("MainMenuScene");
+        }
+    }
+    
 
     /// <summary>
     /// allows switching typing fields with the press of tab button 
@@ -30,10 +53,4 @@ public class ChangeInput : MonoBehaviour
                 next.Select();
         }
     }
-
-    public static void LoadMenuScene()
-    {
-        SceneManager.LoadScene(0);
-    }
-
 }
