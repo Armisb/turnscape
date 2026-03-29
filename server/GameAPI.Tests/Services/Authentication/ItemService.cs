bb121d0c75273f4d8a5c908636ed54c63e021abb
popupro-pro <deividukasisganaitislt@gmail.com>
2026-03-30 00:43:44 +0300

    Finished Unit Testing Item Service

--- server/GameAPI.Tests/Services/Authentication/ItemService.cs
+++ server/GameAPI.Tests/Services/Authentication/ItemService.cs
@@ -11,6 +11,8 @@ using GameAPI.Services.Authentication;
 using GameAPI.Services.Item;
 using shared_lib.ItemDtos;
 using GameAPI.NewFolder.ItemDtos;
+using GameAPI.Services.ItemType;
+using shared_lib.ItemTypeDtos;
 
 namespace GameAPI.Tests;
 
@@ -21,6 +23,7 @@ public class ItemServiceTests : IDisposable
     CreateUserDto createUserDto;
     JwtAuthenticationService authService;
     ItemService itemService;
+    ItemTypeService itemTypeService;
     private readonly ITestOutputHelper output;
 
     public ItemServiceTests(ITestOutputHelper output)
@@ -41,6 +44,7 @@ public class ItemServiceTests : IDisposable
 
         authService = new JwtAuthenticationService(dbContext, configuration);
         itemService = new ItemService(dbContext);
+        itemTypeService = new ItemTypeService(dbContext);
 
         this.output = output;
     }
@@ -86,34 +90,58 @@ public class ItemServiceTests : IDisposable
         Assert.Empty(updatedPos);
     }
 
+    [Fact]
+    public async Task UpdateItemsPositionsWithEmptyInventroyAndNonEmptyList()
+    {
+        GameUser gameUser = await authService.SignupUserAsync(createUserDto);
+        var updatePosDtos = new List<UpdatePosDto>();
+        updatePosDtos.Add(new UpdatePosDto
+        {
+           Id = Guid.NewGuid(),
+           Position = 2,
+           InventoryType = "NewInv"
+        });
+
+        List<UpdatePosDto> updatedPos = await itemService.UpdateItemsPositions(updatePosDtos, gameUser.Id);
+        Assert.Equal(2, updatedPos[0].Position);
+        Assert.Equal("NewInv", updatedPos[0].InventoryType);
+        Assert.Equal(0, dbContext.Items.Count());
+    }
+
 
     public async Task<List<Guid>> InitializeInventoryWithTwoItems(Guid id)
     {
-        output.WriteLine("Initialize inventory");
+        var createArmorTypeDto = new CreateArmorTypeDto
+        {
+            Category = "Chestplate",
+            Name = "Iron",
+            Protection = 10
+        };
+
+        var itemTypeGuid = (await itemTypeService.CreateArmorType(createArmorTypeDto)).Id;
+
         var createItemDto = new CreateItemDto
         {
             InventoryType = "InvType",
             Position = 0,
             Level = 1,
             Health = 10,
-            ItemTypeId = Guid.NewGuid(),
+            ItemTypeId = itemTypeGuid,
             GameUserId = id
         };
 
         List<Guid> guids = new List<Guid>();
         guids.Add((await itemService.CreateItem(createItemDto)).Id);
 
-        createItemDto.ItemTypeId = Guid.NewGuid();
         createItemDto.Position = 1;
         createItemDto.Level = 2;
 
-        await itemService.CreateItem(createItemDto);
         guids.Add((await itemService.CreateItem(createItemDto)).Id);
         return guids;
     }
 
     [Fact]
-    public async Task MoveOneItemToAFreeLocation()
+    public async Task MoveOneItemToAFreeLocationInNewInventory()
     {
         GameUser gameUser = await authService.SignupUserAsync(createUserDto);
         List<Guid> itemGuids = await InitializeInventoryWithTwoItems(gameUser.Id);
@@ -131,13 +159,117 @@ public class ItemServiceTests : IDisposable
         Assert.Equal(updatePosDtos[0].Id, itemUpdates[0].Id);
 
         var item = dbContext.Items.FirstOrDefault(x => x.Id == itemGuids[0]);
-        output.WriteLine(itemGuids[0].ToString());
-        output.WriteLine(dbContext.Items.ToArray()[0].Id.ToString());
-        output.WriteLine(dbContext.Items.ToArray()[1].Id.ToString());
-        output.WriteLine(dbContext.Items.Count().ToString());
         Assert.NotNull(item);
         Assert.Equal(1, item.Level);
         Assert.Equal(2, item.Position);
         Assert.Equal("NewInv", item.InventoryType);
+
+        item = dbContext.Items.FirstOrDefault(x => x.Id == itemGuids[1]);
+        Assert.NotNull(item);
+        Assert.Equal(2, item.Level);
+        Assert.Equal(1, item.Position);
+        Assert.Equal("InvType", item.InventoryType);
+    }
+
+    [Fact]
+    public async Task MoveItemToTakenLocation()
+    {
+        GameUser gameUser = await authService.SignupUserAsync(createUserDto);
+        List<Guid> itemGuids = await InitializeInventoryWithTwoItems(gameUser.Id);
+        var updatePosDtos = new List<UpdatePosDto>();
+        updatePosDtos.Add(new UpdatePosDto
+        {
+           Id = itemGuids[0],
+           Position = 1,
+           InventoryType = "InvType"
+        });
+
+        List<UpdatePosDto> itemUpdates = await itemService.UpdateItemsPositions(updatePosDtos, gameUser.Id);
+        Assert.Equal(1, itemUpdates[0].Position);
+        Assert.Equal("InvType", itemUpdates[0].InventoryType);
+        Assert.Equal(updatePosDtos[0].Id, itemUpdates[0].Id);
+
+        var item = dbContext.Items.FirstOrDefault(x => x.Id == itemGuids[0]);
+        Assert.NotNull(item);
+        Assert.Equal(1, item.Level);
+        Assert.Equal(1, item.Position);
+        Assert.Equal("InvType", item.InventoryType);
+
+        item = dbContext.Items.FirstOrDefault(x => x.Id == itemGuids[1]);
+        Assert.NotNull(item);
+        Assert.Equal(2, item.Level);
+        Assert.Equal(0, item.Position);
+        Assert.Equal("InvType", item.InventoryType);
+    }
+
+    [Fact]
+    public async Task MoveOneItemToAFreePlayerEquipedLocation()
+    {
+        GameUser gameUser = await authService.SignupUserAsync(createUserDto);
+        List<Guid> itemGuids = await InitializeInventoryWithTwoItems(gameUser.Id);
+        var updatePosDtos = new List<UpdatePosDto>();
+        updatePosDtos.Add(new UpdatePosDto
+        {
+           Id = itemGuids[0],
+           Position = 2,
+           InventoryType = "PlayerEquiped"
+        });
+
+        List<UpdatePosDto> itemUpdates = await itemService.UpdateItemsPositions(updatePosDtos, gameUser.Id);
+        Assert.Equal(2, itemUpdates[0].Position);
+        Assert.Equal("PlayerEquiped", itemUpdates[0].InventoryType);
+        Assert.Equal(updatePosDtos[0].Id, itemUpdates[0].Id);
+
+        var item = dbContext.Items.FirstOrDefault(x => x.Id == itemGuids[0]);
+        Assert.NotNull(item);
+        Assert.Equal(1, item.Level);
+        Assert.Equal(2, item.Position);
+        Assert.Equal("PlayerEquiped", item.InventoryType);
+
+        item = dbContext.Items.FirstOrDefault(x => x.Id == itemGuids[1]);
+        Assert.NotNull(item);
+        Assert.Equal(2, item.Level);
+        Assert.Equal(1, item.Position);
+        Assert.Equal("InvType", item.InventoryType);
+    }
+
+    [Fact]
+    public async Task MoveOneItemToATakenPlayerEquipedLocation()
+    {
+        GameUser gameUser = await authService.SignupUserAsync(createUserDto);
+        List<Guid> itemGuids = await InitializeInventoryWithTwoItems(gameUser.Id);
+        var updatePosDtos = new List<UpdatePosDto>();
+        updatePosDtos.Add(new UpdatePosDto
+        {
+           Id = itemGuids[0],
+           Position = 2,
+           InventoryType = "PlayerEquiped"
+        });
+        updatePosDtos.Add(new UpdatePosDto
+        {
+           Id = itemGuids[1],
+           Position = 0,
+           InventoryType = "PlayerEquiped"
+        });
+
+        List<UpdatePosDto> itemUpdates = await itemService.UpdateItemsPositions(updatePosDtos, gameUser.Id);
+        Assert.Equal(2, itemUpdates[0].Position);
+        Assert.Equal("PlayerEquiped", itemUpdates[0].InventoryType);
+        Assert.Equal(updatePosDtos[0].Id, itemUpdates[0].Id);
+        Assert.Equal(0, itemUpdates[1].Position);
+        Assert.Equal("PlayerEquiped", itemUpdates[1].InventoryType);
+        Assert.Equal(updatePosDtos[1].Id, itemUpdates[1].Id);
+
+        var item = dbContext.Items.FirstOrDefault(x => x.Id == itemGuids[0]);
+        Assert.NotNull(item);
+        Assert.Equal(1, item.Level);
+        Assert.Equal(1, item.Position);
+        Assert.Equal("InvType", item.InventoryType);
+
+        item = dbContext.Items.FirstOrDefault(x => x.Id == itemGuids[1]);
+        Assert.NotNull(item);
+        Assert.Equal(2, item.Level);
+        Assert.Equal(0, item.Position);
+        Assert.Equal("PlayerEquiped", item.InventoryType);
     }
 }
