f73e3345aace0b4956cca5640856de2321ec8d42
NojusZeimys <113110331+NojusZeimys@users.noreply.github.com>
2026-04-26 23:27:08 +0300

    Position bug fix

---
+++ server/GameAPI/Helpers/AttackHelpers.cs
@@ -0,0 +1,21 @@
+/* using System;
+using Microsoft.VisualBasic;
+
+namespace GameAPI.Helpers;
+
+public class AttackHelpers
+{
+
+
+    public static int CalcDamageLiteAttac(int[] playerOneStat, int[] playerTwoStat)
+    {
+        int chance = Random.Shared.Next(1, 100);
+        int positiveooutcome = 85;
+        
+        int 
+        
+
+        return 0;
+    }
+}
+ */
